64233338a784c0f83a83ac1df8521ee73a915c07
Mohammad Idrees <mohammadidrees.mohammadi786@gmail.com>
2025-02-26 13:43:13 +0430

    change a sentence

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='font-bold text-4xl text-center'>Hello Deployment</h1>
+      <h1 className='font-bold text-4xl text-center'>Hello Deployment This is a small change</h1>
     </div>
   )
 }
