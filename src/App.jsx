e43274ee24388e013b5bb5dde2bb9112fb4bdb95
Mohammad Idrees <mohammadidrees.mohammadi786@gmail.com>
2025-02-26 13:37:47 +0430

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='font-bold text-4xl text-center'>Hello Deployment</h1>
+    </div>
+  )
+}
+
+export default App
