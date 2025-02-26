a8b9fd7d1c62d2d82ee48f07caa01f9bac6c0877
Mohammad Idrees <mohammadidrees.mohammadi786@gmail.com>
2025-02-26 13:45:32 +0430

    For student understanding

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className='font-bold text-4xl text-center'>Hello Deployment This is a small change</h1>
+      <span>This is a thired change</span>
     </div>
   )
 }
